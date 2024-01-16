273d133c6c8dd46b727ed8462af34199c67eaa42
Ivan Surovegin <givenrous@yandex.ru>
2024-01-16 10:49:49 +0300

    решение

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,61 @@
-import sum from '../basic';
+import Validator from "../app";
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test("Валидация пройдена", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("IVAN");
+  expect(received).toBe(true);
+});
 
-  expect(result).toBe(6);
+test("Валидация пройдена", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("VANYA");
+  expect(received).toBe(true);
+});
+
+test("Валидация не пройдена", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("@123)");
+  expect(received).toBe(false);
+});
+
+test("Начинается с цифры", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("123IVAN");
+  expect(received).toBe(false);
+});
+
+test("Заканчивается цифрой", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("IVAN123");
+  expect(received).toBe(false);
+});
+
+test("Не более трех цифр подряд", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("O1-11-1O");
+  expect(received).toBe(true);
+});
+
+test("Более трех цифр подряд", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("O111-1O");
+  expect(received).toBe(true);
+});
+
+test("Кириллица", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("ИВАН");
+  expect(received).toBe(false);
+});
+
+test("Начинается с тире", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("-IVAN");
+  expect(received).toBe(false);
+});
+
+test("Заканчивается подчеркиванием", () => {
+  const validator = new Validator();
+  const received = validator.validateUsername("IVAN_");
+  expect(received).toBe(false);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default class Validator {
+  validateUsername(name) {
+    this.name = name;
+    return this.name.search(/^([a-z]+(\d{0,3}[_-]+)*\d{0,3})*[a-z]+$/i) !== -1;
+  }
+}
